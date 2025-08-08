cda6bce0d2e35539baebd04b4fea712bcc4327be
Agustin Arenas <arenasagustin7@gmail.com>
2025-08-07 23:16:27 -0300

    refactor: improve code formatting and style in funciones.cpp and main.cpp

--- 00_pointers/funciones.cpp
+++ 00_pointers/funciones.cpp
@@ -2,18 +2,22 @@
 // Created by Roberto Pablo Gomez on 13/03/2025.
 //
 #include <iostream>
+#include "funciones.hpp"
 
-void funcValorPuntero(int* a) {
+void funcValorPuntero(int *a)
+{
     *a = 20;
     std::cout << *a << std::endl;
 }
 
-void funcValorRef(int& a) {
+void funcValorRef(int &a)
+{
     a = 10;
     std::cout << a << std::endl;
 }
 
-void funcValorCopia(int a) {
+void funcValorCopia(int a)
+{
     a = 10;
     std::cout << a << std::endl;
 }
--- 00_pointers/main.cpp
+++ 00_pointers/main.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include "funciones.hpp"
 
-int main() {
+int main()
+{
     int *ptr = nullptr;
     int a = 10;
 
     ptr = &a;
-    std::cout << "Direccion:" << ptr << " Valor: " <<  *ptr << std::endl ;
+    std::cout << "Direccion:" << ptr << " Valor: " << *ptr << std::endl;
 
     *ptr = 20;
-    std::cout << "a: " << a << " *ptr: " <<  *ptr << std::endl ;
+    std::cout << "a: " << a << " *ptr: " << *ptr << std::endl;
 
     int *ptr2 = new int;
-    std::cout << "*ptr2: " <<  *ptr2 << std::endl ;
+    std::cout << "*ptr2: " << *ptr2 << std::endl;
     *ptr2 = 22;
-    std::cout << "*ptr2: " <<  *ptr2 << std::endl ;
+    std::cout << "*ptr2: " << *ptr2 << std::endl;
     delete ptr2;
 
     double x = 5.5;
     double y = 10.2;
     double z;
     double *px = &x;
-    double& ry = y;
+    double &ry = y;
     *px = 3.14;
     z = ry;
     double &r = *px;
@@ -29,21 +30,22 @@ int main() {
     px = &z;
     ry = *px;
 
-    char miString[9] = {'a','c', 'b','d','e','f','g','h','i'};
-    for (int i = 0; i < 9; i++) {
-        std::cout << miString[i] ;
+    char miString[9] = {'a', 'c', 'b', 'd', 'e', 'f', 'g', 'h', 'i'};
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << miString[i];
     }
     std::cout << std::endl;
 
-    int miArray[5] = {1,11,3,4,5};
+    int miArray[5] = {1, 11, 3, 4, 5};
     std::cout << miArray << std::endl;
     std::cout << *miArray << std::endl;
     std::cout << miArray[0] << std::endl;
-    std::cout << *(miArray+3) << std::endl;
+    std::cout << *(miArray + 3) << std::endl;
     std::cout << &miArray << std::endl;
-    std::cout << &miArray+1 << std::endl;
+    std::cout << &miArray + 1 << std::endl;
 
-    int miArray2[5] = {1,11,3,4,5};
+    int miArray2[5] = {1, 11, 3, 4, 5};
     std::cout << miArray2 << std::endl;
 
     miArray2[1] = 123;
