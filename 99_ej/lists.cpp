a9508e7df4244917d582703d1ff03cd8f8c9bbf5
Agustin Arenas <arenasagustin7@gmail.com>
2025-09-09 19:58:12 -0300

    Refactor List and Node classes to implement ListBase as a base class, enhancing structure and functionality

--- 99_ej/lists.cpp
+++ 99_ej/lists.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <stdexcept>
 
-// Implementación del constructor
+// Implementaciones de Node
+// Cosntructores y destructor
 template <typename T>
 Node<T>::Node(T val) : value(val), next(nullptr) {}
+template <typename T>
+Node<T>::~Node() {}
 
 // Implementación del método print
 template <typename T>
@@ -13,17 +16,18 @@ void Node<T>::print()
     std::cout << "[NODO] valor: " << value << ", siguiente: " << next << std::endl;
 }
 
-// Implementación del constructor
+// Implementaciones de ListBase
+// Cosntructores y destructor
 template <typename T>
-List<T>::List() : head(nullptr), lenght(0) {}
+ListBase<T>::ListBase() : head(nullptr), lenght(0) {}
 template <typename T>
-List<T>::List(Node<T> *head) : head(head), lenght(1) {}
+ListBase<T>::ListBase(Node<T> *head) : head(head), lenght(1) {}
 
 // Implementación del destructor
 template <typename T>
-List<T>::~List()
+ListBase<T>::~ListBase()
 {
-    std::cout << "Destruyendo lista..." << std::endl;
+    std::cout << "Destruyendo ..." << std::endl;
 
     while (!isEmpty())
     {
@@ -35,7 +39,7 @@ List<T>::~List()
 
 // Implementación del método push
 template <typename T>
-void List<T>::push(T val)
+void ListBase<T>::push(T val)
 {
     Node<T> *newNode = new Node<T>(val);
     newNode->next = head;
@@ -43,49 +47,62 @@ void List<T>::push(T val)
     lenght++;
 }
 
+// Implementación del método pop
+template <typename T>
+void ListBase<T>::pop(T val)
+{
+    if (isEmpty())
+        throw std::runtime_error("La lista está vacía");
+
+    Node<T> *temp = head;
+    head = head->next;
+    delete temp;
+    lenght--;
+}
+
 // Implementación del método isEmpty
 template <typename T>
-bool List<T>::isEmpty()
+bool ListBase<T>::isEmpty() const
 {
     return head == nullptr;
 }
 
 // Implementación del método getHead
 template <typename T>
-T List<T>::getHead()
+T ListBase<T>::getHead() const
 {
     if (isEmpty())
-        throw std::runtime_error("La lista está vacía");
+        throw std::runtime_error("Está vacía");
 
     return head->value;
 }
 
-// Implementación del método getHead
+// Implementación del método getLenght
 template <typename T>
-int List<T>::getLenght()
+int ListBase<T>::getLenght() const
 {
     return lenght;
 }
 
 // Implementación del método getTail
 template <typename T>
-List<T> *List<T>::getTail()
+ListBase<T> *ListBase<T>::getTail() const
 {
     if (isEmpty())
-        throw std::runtime_error("La lista está vacía");
+        throw std::runtime_error("Está vacía");
 
-    return new List(head->next);
+    return new ListBase<T>(head->next);
 }
 
-// Implementación del método print
+// Implementación del método print de la clase ListBase
 template <typename T>
-void List<T>::print()
+void ListBase<T>::print() const
 {
-    std::cout << "[LISTA] ";
+    std::cout << "[BASE] ";
 
     if (isEmpty())
     {
-        std::cout << "La lista está vacía." << std::endl;
+        std::cout << "La base está vacía." << std::endl;
         return;
     }
 
@@ -93,15 +110,50 @@ void List<T>::print()
     getTail()->print();
 }
 
+// Implementaciones de List
+// Cosntructores y destructor
+template <typename T>
+List<T>::List() : ListBase<T>() {}
+template <typename T>
+List<T>::List(Node<T> *head) : ListBase<T>(head) {}
+template <typename T>
+List<T>::~List() {} // El destructor de la clase base se encarga de la limpieza
+
+// Implementación del método print de la clase List
+template <typename T>
+void List<T>::print() const
+{
+    std::cout << "[LISTA] ";
+
+    if (this->isEmpty())
+    {
+        std::cout << "La lista está vacía." << std::endl;
+        return;
+    }
+
+    this->head->print();
+    this->getTail()->print();
+}
+
+// Implementación del método getTail de la clase List
+template <typename T>
+List<T> *List<T>::getTail() const
+{
+    if (this->isEmpty())
+        throw std::runtime_error("Está vacía");
+
+    return new List<T>(this->head->next);
+}
+
 // Implementación del método get
 template <typename T>
 T List<T>::get(int index)
 {
-    if (index < 0 || index >= lenght)
+    if (index < 0 || index >= this->lenght)
         throw std::runtime_error("El indice no es valido");
 
     int currentIndex = 0;
-    Node<T> *current = head;
+    Node<T> *current = this->head;
 
     while (index != currentIndex)
     {
@@ -116,7 +168,7 @@ T List<T>::get(int index)
 template <typename T>
 List<T> *List<T>::slice(int start, int end)
 {
-    if (start < 0 || end > lenght || start >= end)
+    if (start < 0 || end > this->lenght || start >= end)
         throw std::runtime_error("Índices fuera de rango");
 
     List<T> *slicedList = new List<T>();
@@ -124,7 +176,7 @@ List<T> *List<T>::slice(int start, int end)
     T *tempArray = new T[end - start];
     int tempIndex = 0;
 
-    Node<T> *current = head;
+    Node<T> *current = this->head;
     int currentIndex = 0;
 
     while (current != nullptr && currentIndex < end)
@@ -152,8 +204,11 @@ List<T> *List<T>::slice(int start, int end)
 // Como estamos trabajado con archivos separados, necesitamos instanciar las clases genéricas que vamos a usar
 // Si no lo hacemos, el compilador no generará el código para estas clases y obtendremos errores de "símbolo no definido" durante el enlace
 template class Node<int>;
+template class ListBase<int>;
 template class List<int>;
 template class Node<double>;
+template class ListBase<double>;
 template class List<double>;
 template class Node<std::string>;
+template class ListBase<std::string>;
 template class List<std::string>;
--- 99_ej/lists.hpp
+++ 99_ej/lists.hpp
@@ -8,29 +8,43 @@ struct Node
     T value;       // Valor del nodo
     Node<T> *next; // Puntero al siguiente nodo
     Node(T val);   // Constructor
+    ~Node();       // Destructor
     void print();  // Método para imprimir el nodo
 };
 
-// Definición de la clase Lista
+// Definición de la clase base ListBase
 template <typename T>
-class List
+class ListBase
+{
+public:
+    ListBase();                           // Constructor
+    ListBase(Node<T> *head);              // Constructor con nodo inicial
+    ~ListBase();                          // Destructor
+    void push(T val);                     // Agregar al inicio
+    void pop(T val);                      // Eliminar el primer elemento
+    bool isEmpty() const;                 // Verificar si está vacía
+    T getHead() const;                    // Obtener valor del primer nodo
+    int getLenght() const;                // Obtener el tamaño
+    virtual ListBase<T> *getTail() const; // Obtener la sublista sin el primer nodo
+    virtual void print() const;           // Imprimir la lista
+
+protected:         // Cambiar a protected para que las clases derivadas puedan acceder
+    Node<T> *head; // Puntero al primer nodo
+    int lenght;    // Tamaño de la lista
+};
+
+// Definición de la clase Lista que hereda de ListBase
+template <typename T>
+class List : public ListBase<T>
 {
 public:
     List();                             // Constructor
     List(Node<T> *head);                // Constructor con nodo inicial
     ~List();                            // Destructor
-    void push(T val);                   // Agregar al inicio
-    bool isEmpty();                     // Verificar si está vacía
-    T getHead();                        // Obtener valor del primer nodo
-    int getLenght();                    // Obtener el tamaño
-    List<T> *getTail();                 // Obtener la sublista sin el primer nodo
-    void print();                       // Imprimir la lista
+    void print() const override;        // Imprimir la lista
+    List<T> *getTail() const override;  // Obtener la sublista sin el primer nodo
     T get(int index);                   // Obtener valor en índice
     List<T> *slice(int start, int end); // Obtener una sublista
-
-private:
-    Node<T> *head; // Puntero al primer nodo
-    int lenght;    // Tamaño de la lista
 };
 
 #endif // LISTS_HPP
