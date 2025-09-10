6b1972e8e69a1e308b43f2daa436f36058f00d6c
Agustin Arenas <arenasagustin7@gmail.com>
2025-09-09 21:00:45 -0300

    Clean up whitespace in Node tests for better readability

--- 99_ej/tests/node_tests.cpp
+++ 99_ej/tests/node_tests.cpp
@@ -5,7 +5,7 @@
 TEST(NodeTests, Constructor)
 {
     Node<int> node(42);
-    
+
     EXPECT_EQ(node.value, 42);
     EXPECT_EQ(node.next, nullptr);
 }
@@ -14,14 +14,14 @@ TEST(NodeTests, Linking)
 {
     Node<int> *node1 = new Node<int>(10);
     Node<int> *node2 = new Node<int>(20);
-    
+
     node1->next = node2;
-    
+
     EXPECT_EQ(node1->value, 10);
     EXPECT_EQ(node1->next, node2);
     EXPECT_EQ(node2->value, 20);
     EXPECT_EQ(node2->next, nullptr);
-    
+
     delete node1;
     delete node2;
 }
